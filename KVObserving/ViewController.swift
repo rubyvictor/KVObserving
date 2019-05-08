a858b3149de2770033708ef5a31a83f0bd753dcd
victor lee ming yew <victor_li@flyscoot.com>
2019-05-08 14:46:06 +0800

    Add Observer to TextField Input Change

--- KVObserving/ViewController.swift
+++ KVObserving/ViewController.swift
@@ -17,8 +17,11 @@ class ViewController: UIViewController {
     
     var nameObservationToken: NSKeyValueObservation?
     var ageObservationToken: NSKeyValueObservation?
+    var inputTextObservationToken: NSKeyValueObservation?
     
     @objc let user = User()
+    
+    @objc dynamic var inputText: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,16 +30,36 @@ class ViewController: UIViewController {
             guard let updatedName = change.newValue else { return }
             strongSelf.nameLabel.text = updatedName
         }
+        
+        ageObservationToken = observe(\.user.age, options: .new) { (vc, change) in
+            guard let updatedAge = change.newValue else { return }
+            vc.ageLabel.text = String(updatedAge)
+        }
+        
+        inputTextObservationToken = observe(\.inputText, options: .new) { (vc, change) in
+            guard let updatedInputText = change.newValue as? String else { return }
+            vc.textLabel.text = updatedInputText
+        }
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        nameObservationToken?.invalidate()
+        ageObservationToken?.invalidate()
+    }
+    
+    
     @IBAction func didTapName() {
         user.name = "Victory"
     }
     
     @IBAction func didTapAge() {
+        user.age = 88
     }
     
     @IBAction func textFieldDidChange() {
+        inputText = textField.text
     }
 }
 
